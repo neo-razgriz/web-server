a9e7c8dd7ce0da8abf768447d142b77cdc6e0b4a
Ben Paradise <neo.razgriz@gmail.com>
2025-09-14 23:03:19 +0300

    make more pretty

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,32 @@
 // src/components/Navbar.jsx
 import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link
-import './Navbar.css'; // We will create this CSS file next
+// 1. Import NavLink instead of Link
+import { NavLink } from 'react-router-dom'; 
+import './Navbar.css';
 
 function Navbar() {
   return (
     <nav className="navbar">
       <ul>
         <li>
-          {/* Use <Link to=""> instead of <a href=""> */}
-          <Link to="/">Home</Link>
+          {/* 2. Use NavLink instead of Link */}
+          {/* 3. Add this className function. React Router will pass in { isActive: true }
+               when this link matches the current URL. */}
+          <NavLink 
+            to="/"
+            className={({ isActive }) => (isActive ? 'active-link' : undefined)}
+          >
+            Home
+          </NavLink>
         </li>
         <li>
-          <Link to="/supplements">Supplements</Link>
+          <NavLink 
+            to="/supplements"
+            className={({ isActive }) => (isActive ? 'active-link' : undefined)}
+          >
+            Supplements
+          </NavLink>
         </li>
-        {/* When you want to add a new page (e.g., "About"):
-          1. Create src/pages/AboutPage.jsx
-          2. Add the route in App.jsx (see Step 6)
-          3. Add the link here: <li><Link to="/about">About</Link></li>
-        */}
       </ul>
     </nav>
   );
