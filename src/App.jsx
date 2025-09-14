458758022b47af6b1f0a1408c42aa6057c5129c1
Ben Paradise <neo.razgriz@gmail.com>
2025-09-14 22:06:39 +0300

    remove unnecessary comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-// In your src/App.jsx
 import React from 'react';
 import SupplementSchedule from './components/SupplementSchedule';
-// ... other imports
 
 function App() {
   return (
     <div className="App">
-      {/* ...other parts of your app (navbar, etc) */}
+      {}
       <SupplementSchedule />
     </div>
   );
